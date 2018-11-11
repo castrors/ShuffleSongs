22c3e7256863ce6f0cd46020dd9d15d72a911edd
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2018-11-11 14:53:00 -0200

    Replaces adapter to handle with dataset

--- app/src/main/java/com/castrodev/shufflesongs/ui/SongsAdapter.kt
+++ app/src/main/java/com/castrodev/shufflesongs/ui/SongsAdapter.kt
@@ -1,31 +1,25 @@
 package com.castrodev.shufflesongs.ui
 
-import android.support.v7.recyclerview.extensions.ListAdapter
-import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import com.castrodev.shufflesongs.data.network.response.Song
 
-class SongsAdapter : ListAdapter<Song, RecyclerView.ViewHolder>(SONG_COMPARATOR) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+class SongsAdapter(songsList: List<Song>) : RecyclerView.Adapter<SongViewHolder>() {
+
+    var dataSet: List<Song> = songsList
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getItemCount() = dataSet.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
         return SongViewHolder.create(parent)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val productItem = getItem(position)
-        productItem?.apply {
-            (holder as SongViewHolder).bind(this)
-        }
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = dataSet[position]
+        holder.bind(song)
     }
-
-    companion object {
-        private val SONG_COMPARATOR = object : DiffUtil.ItemCallback<Song>() {
-            override fun areItemsTheSame(oldItem: Song, newItem: Song): Boolean =
-                oldItem.trackName == newItem.trackName
-
-            override fun areContentsTheSame(oldItem: Song, newItem: Song): Boolean =
-                oldItem == newItem
-        }
-    }
-
 }
